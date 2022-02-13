dfaa7cef49e61a23c69dd5ba733699a20cd13db6
fadoghub <80293063+fadoghub@users.noreply.github.com>
2022-02-13 20:22:36 +0330

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -22,7 +22,7 @@ else:
 
 
 @Client.on_inline_query()
-async def inline_search(bot, query: InlineQuery, message):
+async def inline_search(bot, query: InlineQuery):
     await add_user(query.from_user.id)
     me = []
     try:
